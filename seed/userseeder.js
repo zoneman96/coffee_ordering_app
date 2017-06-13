9c53c0e90d4cf2a7c34140aa5cd3cd3b5e62f495
Caleb Calebcorbin Tirzah <calebcorbin@Calebs-MacBook-Pro.local>
2017-06-12 20:21:50 -0500

    Corrected User Seeder

--- seed/userseeder.js
+++ seed/userseeder.js
@@ -1,3 +1,3 @@
 var User = require("../models/user");
 
-User.find(updateOne({email: "admin@admin.com"}, {$set: {"isAdmin": "true"}}))
+User.updateOne({email: "admin@admin.com"}, {$set: {"isAdmin": "true"}})
